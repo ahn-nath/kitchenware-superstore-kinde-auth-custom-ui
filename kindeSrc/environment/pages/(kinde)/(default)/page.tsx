0e97adf3cfcd297c91c5e8fe0759dd17a3a6a0af
ankush <ankush@compose.co.in>
2025-04-05 01:23:52 +0530

    add: a null check for the anchor tag

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -17,44 +17,51 @@ const DefaultPage: React.FC<KindePageEvent> = ({ context, request }) => {
       <div className='container'>
         <main className='login-form-wrapper'>
           <div className='login-form'>
-            <a href={`${process.env.KINDE_SITE_URL}`} className='back-to-site'>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                className='icon icon-tabler icons-tabler-outline icon-tabler-circle-arrow-left'
-              >
-                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-                <path d='M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18' />
-                <path d='M8 12l4 4' />
-                <path d='M8 12h8' />
-                <path d='M12 8l-4 4' />
-              </svg>
-              Back to site
-            </a>
-            <a href={`${process.env.KINDE_SITE_URL}`}>
-              <div className='logo-wrapper'>
-                <picture>
-                  <source
-                    media='(prefers-color-scheme: dark)'
-                    srcSet={getDarkModeLogoUrl()}
-                  />
-                  <img
-                    className='logo'
-                    src={getLogoUrl()}
-                    alt={context.widget.content.logoAlt}
-                    width={152}
-                    height={32}
-                  />
-                </picture>
-              </div>
-            </a>
+            {process?.env?.KINDE_SITE_URL && (
+              <>
+                <a
+                  href={`${process.env.KINDE_SITE_URL}`}
+                  className='back-to-site'
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    width='24'
+                    height='24'
+                    viewBox='0 0 24 24'
+                    fill='none'
+                    stroke='currentColor'
+                    strokeWidth='2'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    className='icon icon-tabler icons-tabler-outline icon-tabler-circle-arrow-left'
+                  >
+                    <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                    <path d='M12 21a9 9 0 1 0 0 -18a9 9 0 0 0 0 18' />
+                    <path d='M8 12l4 4' />
+                    <path d='M8 12h8' />
+                    <path d='M12 8l-4 4' />
+                  </svg>
+                  Back to site
+                </a>
+                <a href={`${process.env.KINDE_SITE_URL}`}>
+                  <div className='logo-wrapper'>
+                    <picture>
+                      <source
+                        media='(prefers-color-scheme: dark)'
+                        srcSet={getDarkModeLogoUrl()}
+                      />
+                      <img
+                        className='logo'
+                        src={getLogoUrl()}
+                        alt={context.widget.content.logoAlt}
+                        width={152}
+                        height={32}
+                      />
+                    </picture>
+                  </div>
+                </a>
+              </>
+            )}
             {context.widget.content.heading && (
               <h2 className='heading'>{context.widget.content.heading}</h2>
             )}
