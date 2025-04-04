b83d14a271abef831f48b49064a935a77f51f70b
ankush <ankush@compose.co.in>
2025-04-05 01:22:33 +0530

    refactor: reorganize environment variables in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,15 +8,26 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: '/(.*)',
         headers: [
           {
-            key: "Content-Security-Policy",
+            key: 'Content-Security-Policy',
             value: "default-src * 'unsafe-inline' 'unsafe-eval';",
           },
         ],
       },
     ];
+  },
+  env: {
+    KINDE_SITE_URL:
+      process.env.KINDE_SITE_URL ??
+      `https://kwss-kitchenwarehouse.frontend.site/`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ??
+      `https://kwss-kitchenwarehouse.frontend.site/`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ??
+      `https://kwss-kitchenwarehouse.frontend.site/`,
   },
 };
 
