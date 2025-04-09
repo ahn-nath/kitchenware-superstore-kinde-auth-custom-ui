c3a3d1a7af101b965eaf6a392ccbe4c852ea9121
ankush <ankush@compose.co.in>
2025-04-09 13:15:17 +0530

    refactor: update Tailwind CSS content paths to include 'kindeSrc' directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
 module.exports = {
   mode: 'jit',
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}','./kindeSrc/**/*.{js,ts,jsx,tsx}'],
 };
