7955d0a516a853f052ada217e1e778793b308aa1
ankush <ankush@compose.co.in>
2025-04-10 21:44:53 +0530

    fix: set dynamic height for side panel image for improved layout adaptability

--- kindeSrc/environment/pages/layout.tsx
+++ kindeSrc/environment/pages/layout.tsx
@@ -199,6 +199,7 @@ export const Layout = ({
               }
               .side-panel-image {
                 width: 100%;
+                height: calc(100vh - 290px);
                 object-fit: cover;
               }
             }
