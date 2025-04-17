681a0884fe235abf0563c0b8c97a26b8c4a03280
ankush <ankush@compose.co.in>
2025-04-18 00:09:47 +0530

    style: comment out console log for request context to clean up output

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -25,7 +25,7 @@ const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
     signupFormTextBottom,
   } = res?.data?.results?.[0]?.data || {};
   const isUserOnLoginOrRegisterPage = request?.route?.flow;
-  console.log('res', context,request)
+  // console.log('res', context,request)
   return (
     <Layout
       context={context}
@@ -93,6 +93,7 @@ const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
 
 // Page Component
 export default async function Page(event: KindePageEvent): Promise<string> {
+  console.log('event',{event})
   const page = await DefaultPage(event);
   return renderToString(page);
 }
