4287e3bf9799b55c5c4a814680dd9f474268480c
ankush <ankush@compose.co.in>
2025-04-19 10:30:03 +0530

    style: remove unused rest parameters from DefaultPage for cleaner code

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -9,7 +9,7 @@ import React from 'react';
 import { renderToString } from 'react-dom/server.browser';
 import Layout from '../../layout';
 
-const DefaultPage: React.FC<KindePageEvent> = async ({ context, request, ...rest }) => {
+const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
   const res = await fetch(
     'https://cdn.builder.io/api/v3/content/login-page-data?apiKey=c065718cfdf849a89015b311c8d3185e&sort.createdDate=-1',
     {
@@ -25,7 +25,7 @@ const DefaultPage: React.FC<KindePageEvent> = async ({ context, request, ...rest
     signupFormTextBottom,
   } = res?.data?.results?.[0]?.data || {};
   const isUserOnLoginOrRegisterPage = request?.route?.flow;
-  console.log('res', rest)
+  console.log('res', context, request)
   return (
     <Layout
       context={context}
