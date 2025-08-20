e05eff21b6dd599ba1ba2d9a66f0ed21bd57f209
Nathaly Toledo <nathaly12toledo@gmail.com>
2025-08-20 16:47:45 -0400

    feat: test 1 - added conditional logic for rendering
    Added conditional logic for rendering based on internal attribute of boolean type.

--- kindeSrc/environment/pages/(kinde)/(default)/page.tsx
+++ kindeSrc/environment/pages/(kinde)/(default)/page.tsx
@@ -10,9 +10,12 @@ import { renderToString } from 'react-dom/server.browser';
 import Layout from '../../layout';
 
 const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
+  const conditionalValue = true // process.env.NEXT_PUBLIC_CONDITIONAL_VALUE || 'Default value';
+
+
   console.log('context', context);
   console.log('request', request);
-
+  
   const res = await fetch(
     'https://cdn.builder.io/api/v3/content/login-page-data?apiKey=c065718cfdf849a89015b311c8d3185e&sort.createdDate=-1',
     {
@@ -36,8 +39,10 @@ const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
       props={res?.data?.results?.[0]?.data}
     >
 
+      {/* Example of conditional rendering based on environment variable */}
+      {conditionalValue && 
       <div className='container'>
-        <h1>Hello this is the Default Kinde page - test</h1>
+        <h1>Hello this is the Default Kinde page - Container 1</h1>
         
         <pre>{JSON.stringify(request, null, 2)}</pre>
         <pre>{JSON.stringify(context, null, 2)}</pre>
@@ -100,6 +105,76 @@ const DefaultPage: React.FC<KindePageEvent> = async ({ context, request }) => {
           </div>
         )}
       </div>
+      }
+      {/* Example of conditional rendering based on environment variable */}
+      {!conditionalValue &&
+            <div className='container'>
+        <h1>Hello this is the Default Kinde page - Container 2</h1>
+        
+        <pre>{JSON.stringify(request, null, 2)}</pre>
+        <pre>{JSON.stringify(context, null, 2)}</pre>
+
+        
+        <p>Redirect URI - Container 2: {request.authUrlParams?.redirectUri || 'Not available'}</p>
+       
+
+        <div className='login-form-wrapper'>
+          {signInFormTextTop && isUserOnLoginOrRegisterPage === 'login' && (
+            <div
+              className='signInFormTextTopText'
+              dangerouslySetInnerHTML={{
+                __html: `${signInFormTextTop}`,
+              }}
+            />
+          )}
+          {signupFormTextTop && isUserOnLoginOrRegisterPage === 'register' && (
+            <div
+              className='signupFormTextTopText'
+              dangerouslySetInnerHTML={{
+                __html: `${signupFormTextTop}`,
+              }}
+            />
+          )}
+          <div className='login-form'>
+            {context.widget.content.heading && (
+              <h2 className='heading'>{context.widget.content.heading}</h2>
+            )}
+            <p className='description'>{context.widget.content.description}</p>
+            {getKindeWidget()}
+          </div>
+          {signInFormTextBottom && isUserOnLoginOrRegisterPage === 'login' && (
+            <div
+              className='signInFormTextBottomText'
+              dangerouslySetInnerHTML={{
+                __html: `${signInFormTextBottom}`,
+              }}
+            />
+          )}
+          {signupFormTextBottom &&
+            isUserOnLoginOrRegisterPage === 'register' && (
+              <div
+                className='signupFormTextBottom'
+                dangerouslySetInnerHTML={{
+                  __html: `${signupFormTextBottom}`,
+                }}
+              />
+            )}
+        </div>
+        {loginPageImage && (
+          <div className='side-panel'>
+            <picture>
+              <img
+                className='side-panel-image'
+                src={loginPageImage}
+                alt='image'
+              />
+            </picture>
+          </div>
+        )}
+      </div>
+      }
+        
+
     </Layout>
   );
 };
